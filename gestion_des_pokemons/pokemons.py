ea04850e834879c276c2ad43bd2e3606afec3c33
Your Name <you@example.com>
2024-05-06 16:59:55 +0200

    pokemons et distances

--- gestion_des_pokemons/pokemons.py
+++ gestion_des_pokemons/pokemons.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 
 # Récupération des data
@@ -9,9 +10,9 @@ pokemons_coordinates = pd.read_csv("../data/pokemon_coordinates.csv",engine='pyt
 
 # Pokémons choisis
 liste_starter = ['Bulbasaur','Charmander','Squirtle']
-liste_poke_choisis = ['Caterpie','Clefairy','Diglett','Eevee',"Farfetch'd",'Geodude','Growlithe','Jigglypuff',
-                  'Machop','Meowth','Nidoran♂','Nidoran♀','Oddish','Pikachu','Ponyta','Psyduck','Rattata',
-                  'Sandshrew','Venonat','Vulpix','Zubat']
+liste_poke_choisis = ['Caterpie','Clefairy','Diglett','Eevee','Ekans',"Farfetch'd",'Geodude','Growlithe',
+                      'Jigglypuff','Machop','Meowth','Oddish','Paras','Pikachu','Ponyta','Psyduck',
+                      'Rattata','Sandshrew','Venonat','Vulpix','Zubat']
 
 # Création du tableau contenant les 21 pokémons choisis et leurs coordonnées
 pokemons_coordinates['id'] = np.arange(998)
@@ -95,16 +96,15 @@ class Caracteristiques_Pokemon:
         return False
         
     def calcul_pts_attaque(self,autre):
-        # prendre défense en compte
-        points_atk_neutre = self.attack
+        points_atk_neutre = round(self.attack - autre.defense * (autre.defense/autre.total))
         ligne = Caracteristiques_Pokemon.liste_types.index(self.type1)
         colonne1 = Caracteristiques_Pokemon.liste_types.index(autre.type1)
         coeff = Caracteristiques_Pokemon.tableau_affinites[ligne][colonne1]
         if autre.type2 in Caracteristiques_Pokemon.liste_types:
             colonne2 = Caracteristiques_Pokemon.liste_types.index(autre.type2)
             coeff *= Caracteristiques_Pokemon.tableau_affinites[ligne][colonne2]
-        points_atk_spe = self.sp_atk * coeff
-        return points_atk_neutre, points_atk_spe
+        points_atk_spe = round((self.sp_atk - autre.sp_def * (autre.sp_def/autre.total))* coeff)
+        return points_atk_neutre, points_atk_spe, coeff
             
     def tour_joueur(self,autre):
         Attaquer = True
@@ -126,17 +126,19 @@ class Caracteristiques_Pokemon:
 
 class Pokemon(Caracteristiques_Pokemon):
     
-    distance_detection = 1
-    
     def __init__(self,ligne_coord):
         ligne_pokemon = dico_array[ligne_coord[0]]
         super().__init__(ligne_pokemon)
         self.coordX = ligne_coord[2]
         self.coordY = ligne_coord[3]
+        self.img_dos = mpimg.imread(f"../interface_graphique/images/images_pokemon/pokemons_finaux/dos/{ligne_coord[0]}.png")
         
     def __str__(self):
         txt2 = f'\nCoordonnées : [{self.coordX}, {self.coordY}]'
         return super().__str__() + txt2
+    
+    def plot_dos(self):
+        plt.imshow(self.img_dos)
         
         
         
@@ -151,17 +153,27 @@ for k in range(len(tableau_caracteristiques_pokemons)):
 dico_poke = {}
 for k in range(len(tableau_pokemons)):
     ligne_coord = tableau_pokemons[k]
-    cle = (ligne_coord[0],ligne_coord[2],ligne_coord[3])
-    dico_poke[cle] = Pokemon(ligne_coord)
-    
+    dico_poke[ligne_coord[0]] = Pokemon(ligne_coord)
+for k in range(len(liste_starter)):
+    ligne_coord = np.array([liste_starter[k],'pokédex','pokédex','pokédex','pokédex'])
+    dico_poke[ligne_coord[0]] = Pokemon(ligne_coord)
     
    
 
 print(dico_objet['Ivysaur'])
-print(dico_objet['Bulbasaur'])
-dico_objet['Ivysaur'].calcul_pts_attaque(dico_objet['Bulbasaur'])
+print(dico_objet['Ivysaur'].calcul_pts_attaque(dico_objet['Bulbasaur']))
 
-print(dico_poke[('Caterpie', 213, 227.632)])
+dico_poke['Caterpie'].plot_dos()
+print(dico_poke['Caterpie'])
 
 
+def calcul_distance_poke(coord,nom_poke):
+    return np.sqrt((coord[0] - dico_poke[nom_poke].coordX)**2 + (coord[1] - dico_poke[nom_poke].coordY)**2)
+    
 
+def revelation(coord):
+    distance_detection = 1
+    for k in range(len(dico_poke)):
+        dist = calcul_distance_poke(coord, nom_poke)
+        
+print(calcul_distance_poke([0,0], 'Caterpie'))
