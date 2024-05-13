3266974afbd710b00e995d971366fe17d7eda35f
MLoeva <you@example.com>
2024-05-13 13:32:41 +0200

    choix_attaque.ui

--- interface_graphique/choix_attaque.py
+++ interface_graphique/choix_attaque.py
@@ -11,14 +11,14 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_choix_attaque(object):
+class Ui_Choix_attaque(object):
     def setupUi(self, Choix_attaque):
         Choix_attaque.setObjectName("Choix_attaque")
         Choix_attaque.resize(741, 212)
         self.image_fond = QtWidgets.QLabel(Choix_attaque)
         self.image_fond.setGeometry(QtCore.QRect(-10, -30, 831, 591))
         self.image_fond.setText("")
-        self.image_fond.setPixmap(QtGui.QPixmap("interface_graphique/images/prairie_et_ciel.png"))
+        self.image_fond.setPixmap(QtGui.QPixmap("images/prairie_et_ciel.png"))
         self.image_fond.setScaledContents(True)
         self.image_fond.setObjectName("image_fond")
         self.bouton_att_spe = QtWidgets.QPushButton(Choix_attaque)
@@ -61,13 +61,20 @@ class Ui_choix_attaque(object):
         font.setPointSize(12)
         self.nbr_degat_att_neutre.setFont(font)
         self.nbr_degat_att_neutre.setObjectName("nbr_degat_att_neutre")
+        self.image_fond.raise_()
+        self.bouton_att_spe.raise_()
+        self.bouton_att_neutre.raise_()
+        self.txt_degats_att_spe.raise_()
+        self.txt_degats_att_neutre.raise_()
+        self.nbr_degat_att_spe.raise_()
+        self.nbr_degat_att_neutre.raise_()
 
         self.retranslateUi(Choix_attaque)
         QtCore.QMetaObject.connectSlotsByName(Choix_attaque)
 
     def retranslateUi(self, Choix_attaque):
         _translate = QtCore.QCoreApplication.translate
-        Choix_attaque.setWindowTitle(_translate("Choix_attaque", "Choisis ton attaque !"))
+        Choix_attaque.setWindowTitle(_translate("Choix_attaque", "Dialog"))
         self.bouton_att_spe.setText(_translate("Choix_attaque", "Attaque spéciale"))
         self.bouton_att_neutre.setText(_translate("Choix_attaque", "Attaque neutre"))
         self.txt_degats_att_spe.setText(_translate("Choix_attaque", "Dégats inffligés :              HP"))
